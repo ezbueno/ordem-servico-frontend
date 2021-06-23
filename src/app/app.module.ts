b3da48f9f4125837d6f1431826642d5dc276b5c9
ezbueno <ezandrobueno@yahoo.com.br>
2021-06-22 21:01:12 -0300

    Gerado o componente HOME

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,9 +25,10 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { HeaderComponent } from './views/components/template/header/header.component';
 import { FooterComponent } from './views/components/template/footer/footer.component';
 import { NavComponent } from './views/components/template/nav/nav.component';
+import { HomeComponent } from './views/components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
