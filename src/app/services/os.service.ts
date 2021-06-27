74fb7a7424bd5796eaca29d6a638e4d7bca3be92
ezbueno <ezandrobueno@yahoo.com.br>
2021-06-26 21:30:36 -0300

    Implementado o SERVICE para Ordem de Serviço

---
+++ src/app/services/os.service.ts
@@ -0,0 +1,48 @@
+import { OrdemServico } from "./../models/os";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Injectable({
+  providedIn: "root",
+})
+export class OsService {
+  baseURL: String = environment.baseURL;
+
+  constructor(private http: HttpClient, private snack: MatSnackBar) {}
+
+  findAll(): Observable<OrdemServico[]> {
+    const URL = this.baseURL + "/os";
+    return this.http.get<OrdemServico[]>(URL);
+  }
+
+  findById(id: any): Observable<OrdemServico> {
+    const URL = `${this.baseURL}/os/${id}`;
+    return this.http.get<OrdemServico>(URL);
+  }
+
+  create(ordemServico: OrdemServico): Observable<OrdemServico> {
+    const URL = this.baseURL + "/os";
+    return this.http.post<OrdemServico>(URL, ordemServico);
+  }
+
+  update(ordemServico: OrdemServico): Observable<OrdemServico> {
+    const URL = `${this.baseURL}/os/${ordemServico.id}`;
+    return this.http.put<OrdemServico>(URL, ordemServico);
+  }
+
+  delete(id: any): Observable<void> {
+    const URL = `${this.baseURL}/os/${id}`;
+    return this.http.delete<void>(URL);
+  }
+
+  message(msg: String): void {
+    this.snack.open(`${msg}`, "OK", {
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 4000,
+    });
+  }
+}
